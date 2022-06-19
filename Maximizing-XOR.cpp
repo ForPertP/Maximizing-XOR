b8c59ec6dbf81e258f1a5f8d06159dee0c907af0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-06-20 00:20:32 +0900

    Update Maximizing-XOR.cpp

--- Maximizing-XOR.cpp
+++ Maximizing-XOR.cpp
@@ -5,3 +5,41 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string l_temp;
+    getline(cin, l_temp);
+
+    int l = stoi(ltrim(rtrim(l_temp)));
+
+    string r_temp;
+    getline(cin, r_temp);
+
+    int r = stoi(ltrim(rtrim(r_temp)));
+
+    int result = maximizingXor(l, r);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
